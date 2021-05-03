da3f8f608f2dd86cc5c55e1d84b903e0dbdd25eb
gili-Katagiri <kt.giri1425@gmail.com>
2021-05-03 16:26:02 +0900

    Set customrc to use fd at fzf.

--- etc/customrc/fzf.sh
+++ etc/customrc/fzf.sh
@@ -1,3 +1,6 @@
+# fzf use fd as default
+export FZF_DEFAULT_COMMAND='fd --type f --follow --hidden --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # fzf default options
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 # comflict with zprezto '**'
