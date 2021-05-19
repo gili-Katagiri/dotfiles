4421d3ef89c120b2af8e3ebff3dcfabaa6e5a51f
gili-Katagiri <kt.giri1425@gmail.com>
2021-05-19 15:37:34 +0900

    Improve fzf config file.

--- etc/customrc/fzf.sh
+++ etc/customrc/fzf.sh
@@ -1,20 +1,26 @@
+# fzf-defult and key-bindings
 # fzf use fd as default
 export FZF_DEFAULT_COMMAND='fd --type f --follow --hidden --exclude .git'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 # fzf default options
-export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
-# comflict with zprezto '**'
-export FZF_COMPLETION_TRIGGER='//'
+export FZF_DEFAULT_OPTS='--height 40% --reverse --border=rounded'
 
+# Ctrl-T is used to shorten the imput of '$(fzf)'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type d --follow"
 # Alt-C options
 {
   # preview tree
-  local opt="--preview 'tree -C {} | head -100'"
+  local opt="--preview 'exa --tree --level 3 --color=always {} | head -100'"
   # auto select 
   opt="$opt --select-1 --exit-0"
   export FZF_ALT_C_OPTS="$opt"
 }
 
+# fzf-completions
+# Policy: NOT to target hidden files
+
+# comflict with zprezto '**'
+export FZF_COMPLETION_TRIGGER='//'
 # fzf-completion does not look for hidden files
 _fzf_compgen_path() {
   fd --follow . "$1"
@@ -28,7 +34,7 @@ _fzf_comprun() {
   shift
 
   case "$command" in
-    cd)     fzf "$@" --preview 'tree -C {} | head -100' ;;
+    cd)     fzf "$@" --preview 'exa --tree --level 3 --color=always {} | head -100' ;;
     *)      fzf "$@" ;;
   esac
 }
