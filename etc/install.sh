e56eb0149c96e649c79610031c5bfd4cc6aec5e6
gili-Katagiri <kt.giri1425@gmail.com>
2021-05-22 14:23:56 +0900

    Setup vue-tab and replace scripts tabs to space.

--- etc/install.sh
+++ etc/install.sh
@@ -11,8 +11,8 @@ echo "Dependency commands:"
 for dcom in ${dependency_commands[@]}; do
     message=$($dcom --version 2>/dev/null)
     if [ "${message}" = "" ]; then
-	echo " !$dcom: Not Found."
-	exit 1
+        echo " !$dcom: Not Found."
+        exit 1
     fi
     echo "  ${message:0:32} ... "
 done
@@ -38,31 +38,31 @@ fi
 if !(type jq > /dev/null 2>&1); then
     # binary install to .local/bin
     (
-	temp_version=1.6
-	echo "Install jq v$temp_version ..."
-	# .local/opt/jq
-	mkdir -p "$LOCAL_OPT_PATH"/jq && cd $_
-	echo -n "Download ... "
-	( curl -sLO https://github.com/stedolan/jq/releases/download/jq-$temp_version/jq-linux64 && \
-	    curl -sLO https://raw.githubusercontent.com/stedolan/jq/master/sig/v$temp_version/sha256sum.txt ) || \
-	# failuer to download, exit this sub-shell
-	( echo "Error: Failed to download from $_" && false ) || exit 1 && \
-	# complete download
-	echo "Complete!"
-	# checksum and chmod and link
-	echo -n "Validation ... "
-	grep jq-linux64 sha256sum.txt | shasum -qa 256 -c - || \
-	( echo "Error: Not match SHA256SUM." && false ) || exit 1 && \
-	echo "checksum 256: Complete!"
-	# post process
-	chmod 755 jq-linux64 && \
-	ln -sfnv $(pwd)/jq-linux64 "$LOCAL_BIN_PATH"/jq
+        temp_version=1.6
+        echo "Install jq v$temp_version ..."
+        # .local/opt/jq
+        mkdir -p "$LOCAL_OPT_PATH"/jq && cd $_
+        echo -n "Download ... "
+        ( curl -sLO https://github.com/stedolan/jq/releases/download/jq-$temp_version/jq-linux64 && \
+            curl -sLO https://raw.githubusercontent.com/stedolan/jq/master/sig/v$temp_version/sha256sum.txt ) || \
+            # failuer to download, exit this sub-shell
+            ( echo "Error: Failed to download from $_" && false ) || exit 1 && \
+            # complete download
+            echo "Complete!"
+        # checksum and chmod and link
+        echo -n "Validation ... "
+            grep jq-linux64 sha256sum.txt | shasum -qa 256 -c - || \
+            ( echo "Error: Not match SHA256SUM." && false ) || exit 1 && \
+            echo "checksum 256: Complete!"
+        # post process
+        chmod 755 jq-linux64 && \
+            ln -sfnv $(pwd)/jq-linux64 "$LOCAL_BIN_PATH"/jq
     )
     if [ $? -ne 0 ]; then
-	echo "Fetal Error: Failed to install 'jq'."
-	exit 1
+        echo "Fetal Error: Failed to install 'jq'."
+        exit 1
     else
-	echo -e "jq installation accomplished!\n"
+        echo -e "jq installation accomplished!\n"
     fi
 fi
 
@@ -82,7 +82,7 @@ EOF
     # enable to call sytra-entry as .local/bin/sytra
     ln -sfnv "${LOCAL_OPT_PATH}"/sytra-docker/sytra-entry.sh "${LOCAL_BIN_PATH}"/sytra
     echo "You may need to call bellow command for extraction stock data,
-    which should be prepared as backup-file (e.g. backup.tar.gz) in previous environment."
+        which should be prepared as backup-file (e.g. backup.tar.gz) in previous environment."
     echo "Command:    \'sytra extract -f backup.tar.gz\'"
 fi
 
@@ -136,35 +136,35 @@ EOF
 # EXA -----
 if !(type exa > /dev/null 2>&1); then
     (
-	echo "Install exa ."
-	mkdir -p "$LOCAL_OPT_PATH"/exa && cd $_
-	curl -sH "Accept: application/vnd.github.v3+json" \
-	    https://api.github.com/repos/ogham/exa/releases/latest \
-	    -o release_info.json
-	
-	zipurl=$(cat release_info.json | jq -r '.assets | map(select( .name | contains("linux-x86_64") )) | .[].browser_download_url')
-	shasumurl=$(cat release_info.json | jq -r '.assets | map(select( .name | contains("SHA1SUMS") )) | .[].browser_download_url')
-	
-	echo -n "Download ... "
-	( curl -sL "$zipurl" -o exa.zip && curl -sL "$shasumurl" -o sha1sum.txt ) || \
-	( echo "Failed to download from $_"; false ) || exit 1 && \
-	echo "Complete!"
-	
-	# get filename from exa.zip: *assumed to be included binary only*
-	echo -n "Validation ... "
-	fname=$(unzip -Z -1 exa.zip)
-	unzip -oq exa.zip && grep $fname sha1sum.txt | shasum -qa 1 -c - || \
-	( echo "Error: Not match SHA1SUM."; false ) || exit 1 && \
-	echo "checksum 1: Complete!"
-	
-	chmod 755 $fname && \
-	ln -sfnv $(pwd)/"$fname" "$LOCAL_BIN_PATH"/exa
+    echo "Install exa ."
+    mkdir -p "$LOCAL_OPT_PATH"/exa && cd $_
+    curl -sH "Accept: application/vnd.github.v3+json" \
+        https://api.github.com/repos/ogham/exa/releases/latest \
+        -o release_info.json
+
+    zipurl=$(cat release_info.json | jq -r '.assets | map(select( .name | contains("linux-x86_64") )) | .[].browser_download_url')
+    shasumurl=$(cat release_info.json | jq -r '.assets | map(select( .name | contains("SHA1SUMS") )) | .[].browser_download_url')
+
+    echo -n "Download ... "
+    ( curl -sL "$zipurl" -o exa.zip && curl -sL "$shasumurl" -o sha1sum.txt ) || \
+    ( echo "Failed to download from $_"; false ) || exit 1 && \
+        echo "Complete!"
+
+    # get filename from exa.zip: *assumed to be included binary only*
+    echo -n "Validation ... "
+    fname=$(unzip -Z -1 exa.zip)
+    unzip -oq exa.zip && grep $fname sha1sum.txt | shasum -qa 1 -c - || \
+        ( echo "Error: Not match SHA1SUM."; false ) || exit 1 && \
+        echo "checksum 1: Complete!"
+
+    chmod 755 $fname && \
+        ln -sfnv $(pwd)/"$fname" "$LOCAL_BIN_PATH"/exa
     )
     if [ $? -ne 0 ]; then
-	echo "Error: Failed to install 'exa'."
-	echo "You should install manually or via package manager."
+        echo "Error: Failed to install 'exa'."
+        echo "You should install manually or via package manager."
     else
-	echo -e "exa installation accomplished!\n"
+        echo -e "exa installation accomplished!\n"
     fi
 fi
 
@@ -201,21 +201,21 @@ fi
 # poetry -----
 if !(type poetry > /dev/null 2>&1); then
     (
-	echo "Install poetry ... "
-	export POETRY_HOME="$LOCAL_OPT_PATH/poetry"
-	mkdir -p "$POETRY_HOME" && cd $_
-	echo -n "Download and Install ... "
-	curl -sSLO https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py || \
-	( echo "Failed to download from $_"; false ) || exit 1 && \
-	no | python3 get-poetry.py --no-modify-path || \
-	( echo "Failed to install"; false ) || exit 1 && \
-	echo "Complete!"
-	ln -sfnv $(pwd)/bin/poetry "$LOCAL_BIN_PATH"/poetry
+        echo "Install poetry ... "
+        export POETRY_HOME="$LOCAL_OPT_PATH/poetry"
+        mkdir -p "$POETRY_HOME" && cd $_
+        echo -n "Download and Install ... "
+        curl -sSLO https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py || \
+            ( echo "Failed to download from $_"; false ) || exit 1 && \
+            no | python3 get-poetry.py --no-modify-path || \
+            ( echo "Failed to install"; false ) || exit 1 && \
+            echo "Complete!"
+            ln -sfnv $(pwd)/bin/poetry "$LOCAL_BIN_PATH"/poetry
     )
     if [ $? -ne 0 ]; then
-	echo "Error: Failed to install 'poetry'. "
-	echo "You should install manually or via package manager."
+        echo "Error: Failed to install 'poetry'. "
+        echo "You should install manually or via package manager."
     else
-	echo -e "poetry installation accomplished!\n"
+        echo -e "poetry installation accomplished!\n"
     fi
 fi
